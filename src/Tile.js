ef142ef0e3d545c7abbe338f7e009236bf88f0f2
Mehul <mehulg8142@gmail.com>
2024-08-26 01:31:32 +0530

    Haha

--- src/Tile.js
+++ src/Tile.js
@@ -6,11 +6,16 @@ export default function Tile(props) {
 
 
 
-    if (props.clicked===-1 || props.clicked===props.id) { 
+    if (props.clicked===-1) { 
         return (
         <button onClick={ () => {props.setClicked(props.id);}}>
-            <Content click={0}  name={props.name} image={props.image} info={props.info}/>
+            <Content click={1}  name={props.name} image={props.image} info={props.info}/>
         </button>);
+    } else if (props.clicked===props.id) {
+        return (
+            <button onClick={ () => {props.setClicked(-1);}}>
+                <Content click={0}  name={props.name} image={props.image} info={props.info}/>
+            </button>);
     } else {
         return (<button onClick={ () => {props.setClicked(props.id);}}>
             <Content click={1} name={props.name} image={props.image} info={props.info}/>
